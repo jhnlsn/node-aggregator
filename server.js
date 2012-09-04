965e560a5a35c3572d66aa3a4a43882436bd1987
johnymonster <jnelson11@gmail.com>
2012-09-04 08:27:23 -0400

    Moving feed loading logic to a module adding interval timer
    My first attempt at writing a module in nodejs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 	, _ = require('underscore')
-	, feeds = require('feed-read');
+	, feeds = require('./lib/aggregator');
 
 var app = express();
 
@@ -11,26 +11,7 @@ app.use(require('connect-assets')());
 app.use(express.static(__dirname + '/public'));
 
 
-var news_feeds = {};
-
-feeds('http://news.ycombinator.com/rss',function(err, articles){
-        if (err) throw err;
-        news_feeds.hn = articles;
-});
-
-feeds('http://reddit.com/.rss',function(err,feed){
-	news_feeds.reddit = feed;
-});
-
-feeds('http://digg.com/rss/topstories.xml', function(err,feed){
-	news_feeds.digg = feed;
-});
-
-feeds('http://9gag.com/rss/site/feed.rss', function(err,feed){
-	console.log(feed);
-	news_feeds.gag = feed;
-});
-
+var news_feeds = feeds();
 
 app.get('/',function(req,res){
 	res.render('index',{feeds: news_feeds});
